21b6175fba24dcb6c09c5e3e3bb7e2a80aa55dd6
yanrongbao <yanrongbao1566@163.com>
2021-05-06 23:54:15 +0800

    style: 用Row布局页面

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,6 +1,38 @@
+import { useAuth } from "context/auth-context";
 import React from "react";
 import { ProjectListScreen } from "screens/project-list";
+import { Button } from "antd";
+import styled from "@emotion/styled";
+import { Row } from "components/libs";
 
 export const AuthenticatedApp = () => {
-  return <ProjectListScreen />;
+  const { logout } = useAuth();
+  return (
+    <Container>
+      <Header between>
+        <HeaderLeft gap>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <Button onClick={logout}>登出</Button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+
+const Header = styled(Row)``;
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
+const Main = styled.main``;
